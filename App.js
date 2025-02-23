629acd90d590d659efc14fd317f027620f493e83
AmitPanda457 <amitpanda457@gmail.com>
2025-02-23 14:34:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import InstanstEverything from "./components/instanstEverything";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <InstanstEverything />
+    </div>
+  );
+}
+
+export default App;
